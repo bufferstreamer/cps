8d23c56aee3d9a18fa5c5170d48a92f355d82b70
LN <517631523@qq.com>
2022-08-23 12:59:27 +0800

    启动文件修改

--- cps-admin/src/main/java/com/cps/CPSApplication.java
+++ cps-admin/src/main/java/com/cps/CPSApplication.java
@@ -16,16 +16,5 @@ public class CPSApplication
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(CPSApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  集采启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
-
     }
 }
