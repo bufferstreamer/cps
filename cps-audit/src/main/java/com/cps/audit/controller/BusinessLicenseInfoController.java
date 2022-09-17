4c65fdd20c00a035deab3559398acdba2e2fc8aa
LJMqwq <253433595@qq.com>
2022-09-17 16:48:34 +0800

    将营业范围改成了双下拉框

--- cps-audit/src/main/java/com/cps/audit/controller/BusinessLicenseInfoController.java
+++ cps-audit/src/main/java/com/cps/audit/controller/BusinessLicenseInfoController.java
@@ -7,18 +7,24 @@ import com.cps.audit.service.IBusinessLicenseInfoService;
 import com.cps.common.annotation.Log;
 import com.cps.common.core.controller.BaseController;
 import com.cps.common.core.domain.AjaxResult;
+import com.cps.common.core.domain.Ztree;
+import com.cps.common.core.domain.entity.SysDept;
 import com.cps.common.core.page.TableDataInfo;
 import com.cps.common.enums.BusinessType;
 import com.cps.common.utils.DateUtils;
 import com.cps.common.utils.ShiroUtils;
 import com.cps.common.utils.poi.ExcelUtil;
 import com.cps.common.utils.uuid.CpsIdUtils;
+import com.cps.system.service.ISysDeptService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -36,6 +42,8 @@ public class BusinessLicenseInfoController extends BaseController {
     private IBusinessLicenseInfoService businessLicenseInfoService;
     @Autowired
     private IAuditDocumentsService auditDocumentsService;
+    @Autowired
+    private ISysDeptService sysDeptService;
 
     @RequiresPermissions("audit:businessLicenseManage:view")
     @GetMapping()
@@ -72,8 +80,36 @@ public class BusinessLicenseInfoController extends BaseController {
      * 新增小商超审核管理
      */
     @GetMapping("/add")
-    public String add() {
+    public String add(ModelMap map)
+    {
+        AddListToMap(map);
         return prefix + "/add";
+    }
+
+    //将下拉框的信息加入Map
+    private void AddListToMap(ModelMap map){
+        List<String> typeNameList=new ArrayList<>();
+        List<List<SysDept>> scopeList=new ArrayList<>();
+
+        List<SysDept> typeList = GetChildrenByParentId(100);
+        for (SysDept type:typeList)
+        {
+            String typeName=type.getDeptName();
+            List<SysDept> tempList = GetChildrenByParentId(type.getDeptId());
+            typeNameList.add(typeName);
+            scopeList.add(tempList);
+        }
+
+        map.put("typeNameList",typeNameList);
+        map.put("scopeList",scopeList);
+    }
+
+
+    private List<SysDept> GetChildrenByParentId(long parentId){
+        SysDept dept = new SysDept();
+        dept.setParentId(parentId);
+
+        return sysDeptService.selectDeptList(dept);
     }
 
     /**
@@ -83,10 +119,20 @@ public class BusinessLicenseInfoController extends BaseController {
     @Log(title = "小商超审核管理", businessType = BusinessType.INSERT)
     @PostMapping("/add")
     @ResponseBody
-    public AjaxResult addSave(BusinessLicenseInfo businessLicenseInfo) {
+    public AjaxResult addSave(BusinessLicenseInfo businessLicenseInfo, HttpServletRequest request) {
         businessLicenseInfo.setBusinessAuditDocumentId(CpsIdUtils.createChecklistId('2'));//此处应该利用用户id从数据库查找
         businessLicenseInfo.setCreateDatetime(DateUtils.parseDate(DateUtils.getTime()));
+
+        EditBusinessScope(businessLicenseInfo, request);
         return toAjax(businessLicenseInfoService.insertBusinessLicenseInfo(businessLicenseInfo));
+    }
+
+    //对BusinessScope这一字段做处理
+    private void EditBusinessScope(BusinessLicenseInfo businessLicenseInfo, HttpServletRequest request){
+        String type = request.getParameter("businessType");
+        String scope = businessLicenseInfo.getBusinessScope();
+
+        businessLicenseInfo.setBusinessScope(type+":"+scope);
     }
 
     /**
@@ -94,9 +140,17 @@ public class BusinessLicenseInfoController extends BaseController {
      */
     @RequiresPermissions("audit:businessLicenseManage:edit")
     @GetMapping("/edit/{businessAuditDocumentId}")
-    public String edit(@PathVariable("businessAuditDocumentId") String businessAuditDocumentId, ModelMap mmap) {
+    public String edit(@PathVariable("businessAuditDocumentId") String businessAuditDocumentId, ModelMap map) {
+
         BusinessLicenseInfo businessLicenseInfo = businessLicenseInfoService.selectBusinessLicenseInfoByBusinessAuditDocumentId(businessAuditDocumentId);
-        mmap.put("businessLicenseInfo", businessLicenseInfo);
+        String[] scopeArr=businessLicenseInfo.getBusinessScope().split(":");
+        String type = scopeArr[0];
+        String scope = scopeArr[1];
+        businessLicenseInfo.setBusinessScope(scope);
+
+        map.put("businessLicenseInfo", businessLicenseInfo);
+        map.put("businessType", type);
+        AddListToMap(map);
         return prefix + "/edit";
     }
 
@@ -107,10 +161,12 @@ public class BusinessLicenseInfoController extends BaseController {
     @Log(title = "小商超审核管理", businessType = BusinessType.UPDATE)
     @PostMapping("/edit")
     @ResponseBody
-    public AjaxResult editSave(BusinessLicenseInfo businessLicenseInfo) {
+    public AjaxResult editSave(BusinessLicenseInfo businessLicenseInfo, HttpServletRequest request) {
         AuditDocuments auditDocuments = auditDocumentsService.selectAuditDocumentsByChecklistId(businessLicenseInfo.getBusinessAuditDocumentId());
         auditDocuments.setAuditStatus("1");
         auditDocumentsService.updateAuditDocuments(auditDocuments);
+
+        EditBusinessScope(businessLicenseInfo, request);
 
         return toAjax(businessLicenseInfoService.updateBusinessLicenseInfo(businessLicenseInfo));
     }
