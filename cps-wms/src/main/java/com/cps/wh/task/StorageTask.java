a7f47aa3a80e3e3dd5e648d4534469e60e180b0a
happyness1001 <Eternal_2017@163.com>
2022-09-25 14:49:29 +0800

    修改浏览效果

--- cps-wms/src/main/java/com/cps/wh/task/StorageTask.java
+++ cps-wms/src/main/java/com/cps/wh/task/StorageTask.java
@@ -6,9 +6,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * @description:
- * @author: miki
- * @modified By：注意：本内容仅限于湖南索孚威尔科技有限公司内部传阅，禁止外泄以及用于其他的商业目的
- * @time: 2021-06-21 20:51
+ * @author: cps
  */
 @Component("storageTask")
 public class StorageTask {
