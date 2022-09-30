b9f978319bccf2f53d7c02af7d9950cfc74e0a44
LN <517631523@qq.com>
2022-09-30 11:01:46 +0800

    邮件修改

--- cps-user/src/main/java/com/cps/user/domain/Category.java
+++ cps-user/src/main/java/com/cps/user/domain/Category.java
@@ -1,9 +1,9 @@
 package com.cps.user.domain;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import com.cps.common.annotation.Excel;
 import com.cps.common.core.domain.BaseEntity;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 /**
  * 【请填写功能名称】对象 category
@@ -11,69 +11,71 @@ import com.cps.common.core.domain.BaseEntity;
  * @author cps
  * @date 2022-09-21
  */
-public class Category extends BaseEntity
-        {
-private static final long serialVersionUID=1L;
+public class Category extends BaseEntity {
+    private static final long serialVersionUID = 1L;
 
-    /** $column.columnComment */
+    /**
+     * $column.columnComment
+     */
     private String categoryId;
 
-    /** $column.columnComment */
-            @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    /**
+     * $column.columnComment
+     */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
     private String categoryLevel;
 
-    /** $column.columnComment */
-            @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    /**
+     * $column.columnComment
+     */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
     private String parentId;
 
-    /** $column.columnComment */
-            @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    /**
+     * $column.columnComment
+     */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
     private String categoryName;
 
-    public void setCategoryId(String categoryId)
-            {
-            this.categoryId = categoryId;
-            }
+    public String getCategoryId() {
+        return categoryId;
+    }
 
-    public String getCategoryId()
-            {
-            return categoryId;
-            }
-    public void setCategoryLevel(String categoryLevel)
-            {
-            this.categoryLevel = categoryLevel;
-            }
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
 
-    public String getCategoryLevel()
-            {
-            return categoryLevel;
-            }
-    public void setParentId(String parentId)
-            {
-            this.parentId = parentId;
-            }
+    public String getCategoryLevel() {
+        return categoryLevel;
+    }
 
-    public String getParentId()
-            {
-            return parentId;
-            }
-    public void setCategoryName(String categoryName)
-            {
-            this.categoryName = categoryName;
-            }
+    public void setCategoryLevel(String categoryLevel) {
+        this.categoryLevel = categoryLevel;
+    }
 
-    public String getCategoryName()
-            {
-            return categoryName;
-            }
+    public String getParentId() {
+        return parentId;
+    }
 
-@Override
-public String toString(){
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("categoryId",getCategoryId())
-            .append("categoryLevel",getCategoryLevel())
-            .append("parentId",getParentId())
-            .append("categoryName",getCategoryName())
-        .toString();
-        }
-        }
+    public void setParentId(String parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("categoryId", getCategoryId())
+                .append("categoryLevel", getCategoryLevel())
+                .append("parentId", getParentId())
+                .append("categoryName", getCategoryName())
+                .toString();
+    }
+}
