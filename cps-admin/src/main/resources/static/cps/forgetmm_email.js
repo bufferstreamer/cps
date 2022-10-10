5096db9eacfbed8528acc1fa585a31d7be14d259
bubu <78036577+bubu-w@users.noreply.github.com>
2022-10-10 17:54:59 +0800

    重置密码功能

---
+++ cps-admin/src/main/resources/static/cps/forgetmm_email.js
@@ -0,0 +1,118 @@
+$("#getVerifyCode").click(function (){
+    sendEmail($("#getVerifyCode"));
+});
+
+// function sleep(ms) {
+//     let start = Date.now()
+//     let end = start + ms
+//     while(true) {
+//         if(Date.now() > end) {
+//             return
+//         }
+//     }
+// }
+//用ajax提交到后台的发送邮件接口
+function sendEmail(obj){
+    console.log(ctx)
+    var email = $.common.trim($("input[name='userMail']").val());
+    var result = isEmail();
+    if(result) {
+        $.ajax({
+            url: ctx + "sendEmailCode",
+            data:{email:email},
+            dataType:"json",
+            type:"post",
+            async : true,
+            cache : false,
+            // success:function(res){
+            //     debugger;
+            //     if(res){
+            //         alert("验证码发送成功");
+            //     }else{
+            //
+            //     }
+            // },
+            beforeSend: function(){
+                $.modal.loading("验证码发送中，请稍后-ˏˋෆ( ˶'ᵕ'˶)ෆˎˊ-");
+            },
+            success: function (r) {
+                if (r.code == web_status.SUCCESS) {
+                    $.modal.msgSuccess(r.msg);
+                } else {
+                    $.modal.msgError(r.msg);
+                }
+                $.modal.closeLoading();
+            },
+        })
+        setTime(obj);//开始倒计时
+    }else{
+        $.modal.msgWarning("请输入有效的邮箱地址");
+    }
+}
+
+function sendVerifyCode(obj){
+    var email = $.common.trim($("input[name='userMail']").val());
+    var emailVerifyCode = $.common.trim($("input[name='verifyCode']").val());
+    var result = isEmail();
+    if(result) {
+        $.ajax({
+            url: ctx + "checkEmailVerifyCode",
+            data:{emailVerifyCode:emailVerifyCode},
+            dataType:"json",
+            type:"post",
+            async : false,
+            cache : false,
+            // success:function(res){
+            //     debugger;
+            //     if(res){
+            //         alert("验证码发送成功");
+            //     }else{
+            //
+            //     }
+            // },
+            success: function (r) {
+                if (r.code == web_status.SUCCESS) {
+                    $.modal.msgSuccess(r.msg);
+                    setTimeout(function () {
+                        // 这里就是处理的事件
+                        location.href = ctx + 'changemm?email='+email;
+                    }, 1000);
+
+                } else {
+                    $.modal.msgError(r.msg);
+                }
+            }
+        })
+    }
+}
+
+
+
+
+//60s倒计时实现逻辑
+var countdown = 60;
+function setTime(obj) {
+    if (countdown == 0) {
+        obj.prop('disabled', false);
+        obj.text("获取验证码");
+        countdown = 60;//60秒过后button上的文字初始化,计时器初始化;
+        return;
+    } else {
+        obj.prop('disabled', true);
+        obj.text("("+countdown+"s)后重新发送") ;
+        countdown--;
+    }
+    setTimeout(function() { setTime(obj) },1000) //每1000毫秒执行一次
+}
+
+
+//校验邮箱是否合法
+function isEmail(){
+    var email = $.common.trim($("input[name='userMail']").val());
+    var reg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    if(!reg.test(email)){
+        return false;
+    }else{
+        return true;
+    }
+}
---
+++ cps-system/src/main/java/com/cps/system/service/impl/SysEmailServiceImpl.java
@@ -0,0 +1,67 @@
+package com.cps.system.service.impl;
+
+import cn.hutool.extra.mail.MailUtil;
+import com.cps.system.service.ISysEmailService;
+import org.thymeleaf.TemplateEngine;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import java.io.File;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class SysEmailServiceImpl implements ISysEmailService {
+
+    private final TemplateEngine templateEngine;
+
+    /**
+     * 发送邮件
+     *
+     * @param to      目标邮箱
+     * @param subject 标题
+     * @param content 内容
+     */
+    public void send(String to, String subject, String content) {
+        MailUtil.send(to, subject, content, false);
+    }
+
+    /**
+     * 发送邮件(带附件)
+     *
+     * @param to      目标邮箱
+     * @param subject 标题
+     * @param content 内容
+     * @param files   附件（可选）
+     */
+    public void send(String to, String subject, String content, File... files) {
+        MailUtil.send(to, subject, content, false, files);
+    }
+
+    /**
+     * 发送邮件-读取自定义模板
+     *
+     * @param to      目标邮箱
+     * @param subject 标题
+     * @param context 内容
+     */
+    public void send(String to, String subject, Context context) {
+        String template = templateEngine.process("verifyEmailCode", context);
+        MailUtil.send(to, subject, template, true);
+    }
+
+
+    /**
+     * 发送邮件-读取自定义模板（带附件）
+     *
+     * @param to      目标邮箱
+     * @param subject 标题
+     * @param context 内容
+     * @param files   附件
+     */
+    public void send(String to, String subject, Context context, File... files) {
+        String template = templateEngine.process("verifyEmailCode", context);
+        MailUtil.send(to, subject, template, true, files);
+    }
+}
