356fe2951a34f2c4872a6c3ccf2fbaac690950a3
LJMqwq <253433595@qq.com>
2022-10-31 21:38:20 +0800

    提交本地代码

--- cps-bid/src/main/java/com/cps/bid/service/impl/CentralizedPurchaseRecordServiceImpl.java
+++ cps-bid/src/main/java/com/cps/bid/service/impl/CentralizedPurchaseRecordServiceImpl.java
@@ -4,16 +4,12 @@ import com.cps.bid.domain.CentralizedPurchaseRecord;
 import com.cps.bid.mapper.CentralizedPurchaseRecordMapper;
 import com.cps.bid.service.ICentralizedPurchaseRecordService;
 import com.cps.common.core.text.Convert;
-import com.cps.common.json.JSON;
 import com.cps.common.utils.DateUtils;
 import com.cps.common.utils.ShiroUtils;
 import com.cps.common.utils.uuid.IdUtils;
-import com.cps.fabric.bean.CAEnrollment;
-import com.cps.fabric.service.SDKService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,9 +22,6 @@ import java.util.List;
 public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchaseRecordService {
     @Autowired
     private CentralizedPurchaseRecordMapper centralizedPurchaseRecordMapper;
-
-    @Autowired
-    private SDKService sdkService;
     /**
      * 查询集中采购记录
      *
@@ -37,18 +30,6 @@ public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchas
      */
     @Override
     public CentralizedPurchaseRecord selectCentralizedPurchaseRecordByCentralizedPurchaseRecordId(String centralizedPurchaseRecordId) {
-//        try{
-//            String jsonStr = sdkService.queryChainCode("query",new String[]{centralizedPurchaseRecordId});
-//
-//            CentralizedPurchaseRecord record = JSON.unmarshal(jsonStr,CentralizedPurchaseRecord.class);
-//            System.out.println(record);
-//            return record;
-//        }
-//        catch (Exception e){
-//            System.out.println(e.toString());
-//            return null;
-//        }
-
         return centralizedPurchaseRecordMapper.selectCentralizedPurchaseRecordByCentralizedPurchaseRecordId(centralizedPurchaseRecordId);
     }
 
@@ -60,36 +41,6 @@ public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchas
      */
     @Override
     public List<CentralizedPurchaseRecord> selectCentralizedPurchaseRecordList(CentralizedPurchaseRecord centralizedPurchaseRecord){
-        try{
-            if (sdkService.fabricClient==null){
-                sdkService.Init();
-                //sdkService.chainCodeInstall();
-                //sdkService.initChainCode();
-            }
-        }
-        catch (Exception e){
-            System.out.println(e.toString());
-        }
-
-//        List<CentralizedPurchaseRecord> list=new ArrayList<>();
-//        try{
-//            for (String jsonStr:sdkService.queryChainCode("queryByRange",new String[]{"a","z"}).split("\\|"))
-//            {
-//                list.add(JSON.unmarshal(jsonStr,CentralizedPurchaseRecord.class));
-//            }
-//
-//            for (String jsonStr:sdkService.queryChainCode("queryByRange",new String[]{"0","9"}).split("\\|"))
-//            {
-//                list.add(JSON.unmarshal(jsonStr,CentralizedPurchaseRecord.class));
-//            }
-//        }
-//        catch (Exception e){
-//            System.out.println(e.toString());
-//        }
-//
-//
-//        return list;
-
         return centralizedPurchaseRecordMapper.selectCentralizedPurchaseRecordList(centralizedPurchaseRecord);
     }
 
@@ -105,16 +56,6 @@ public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchas
         centralizedPurchaseRecord.setSupplierId(ShiroUtils.getUserId());
         centralizedPurchaseRecord.setCentralizedPurchaseRecordTime(DateUtils.getNowDate());
 
-//        try{
-//            String jsonStr= JSON.marshal(centralizedPurchaseRecord);
-//            sdkService.chainCodeInvoke("add",new String[]{centralizedPurchaseRecord.getCentralizedPurchaseRecordId(),jsonStr});
-//            return 1;
-//        }
-//        catch (Exception e){
-//            System.out.println(e.toString());
-//            return 0;
-//        }
-
         return centralizedPurchaseRecordMapper.insertCentralizedPurchaseRecord(centralizedPurchaseRecord);
     }
 
@@ -126,17 +67,6 @@ public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchas
      */
     @Override
     public int updateCentralizedPurchaseRecord(CentralizedPurchaseRecord centralizedPurchaseRecord) {
-//        try{
-//            System.out.println(centralizedPurchaseRecord);
-//            String jsonStr= JSON.marshal(centralizedPurchaseRecord);
-//            sdkService.chainCodeInvoke("update",new String[]{centralizedPurchaseRecord.getCentralizedPurchaseRecordId(),jsonStr});
-//            return 1;
-//        }
-//        catch (Exception e){
-//            System.out.println(e.toString());
-//            return 0;
-//        }
-
         return centralizedPurchaseRecordMapper.updateCentralizedPurchaseRecord(centralizedPurchaseRecord);
     }
 
@@ -148,20 +78,6 @@ public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchas
      */
     @Override
     public int deleteCentralizedPurchaseRecordByCentralizedPurchaseRecordIds(String centralizedPurchaseRecordIds) {
-//        String[] idArray = Convert.toStrArray(centralizedPurchaseRecordIds);
-//        for (String centralizedPurchaseRecordId:idArray)
-//        {
-//            try{
-//                sdkService.chainCodeInvoke("remove",new String[]{centralizedPurchaseRecordId});
-//            }
-//            catch (Exception e){
-//                System.out.println(e.toString());
-//                return 0;
-//            }
-//        }
-//
-//        return idArray.length;
-
         return centralizedPurchaseRecordMapper.deleteCentralizedPurchaseRecordByCentralizedPurchaseRecordIds(Convert.toStrArray(centralizedPurchaseRecordIds));
     }
 
@@ -173,15 +89,6 @@ public class CentralizedPurchaseRecordServiceImpl implements ICentralizedPurchas
      */
     @Override
     public int deleteCentralizedPurchaseRecordByCentralizedPurchaseRecordId(String centralizedPurchaseRecordId) {
-//        try{
-//            sdkService.chainCodeInvoke("remove",new String[]{centralizedPurchaseRecordId});
-//            return 1;
-//        }
-//        catch (Exception e){
-//            System.out.println(e.toString());
-//            return 0;
-//        }
-
         return centralizedPurchaseRecordMapper.deleteCentralizedPurchaseRecordByCentralizedPurchaseRecordId(centralizedPurchaseRecordId);
     }
 }
